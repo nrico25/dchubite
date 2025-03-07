6919c391ee33efe0bac0f2032ccb96ca62cd4264
Miqdam <151706082+xMiqdam@users.noreply.github.com>
2025-03-07 11:07:25 +0700

    style:[MR] widgets

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:tadchubite/widget/button.dart';
+import 'package:tadchubite/widget/color.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      body: Center(
+        child: Center(
+          child: Column(children: [Text('asu')],),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widget/color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+const yellow = Color(0xFFFFC926); 
+const darkBlue = Color(0xFFFF2A3256); 
+const blue = Color(0xFFFF2098BC); 
+const lightBlue = Color(0xFFFF2098BC); 
+const green = Color(0xFFFF3CAE30); 
+const red = Color(0xFFFFD41F1F); 
+const black = Color(0000); 
+const white = Color(0xFFFFFF); 
+const lightGray = Color(0xFFFFF7F8FA); 
